01095cfe4ae921cf82ee603fbad860b5890359de
Enrique Arce <enrique7arce.eaa@gmail.com>
2023-01-19 13:07:42 -0400

    Creacion de lista y verbos POST y DELETE

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,20 +13,43 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
+    //This is the constructor method
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        //We can need that data enter through this method so that this data live on memory
+        if(ListWeatherForecast == null || !ListWeatherForecast.Any())
+        {
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        //As this method only needs to show data, only we return the list
+        return ListWeatherForecast;
+    }
+
+    [HttpPost(Name = "PostWeatherForecast")]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForecast.Add(weatherForecast);
+        return Ok();
+    }
+
+    [HttpDelete(Name = "DeleteWeatherForecast")]
+    public IActionResult Delete(int index)
+    {
+        ListWeatherForecast.RemoveAt(index);
+        return Ok();
     }
 }
